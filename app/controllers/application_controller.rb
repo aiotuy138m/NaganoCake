9adebc182605ac83eb0edc7100f6c52bcdce60f3
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-02-15 15:42:23 +0000

    [WIP] fix signin-login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    customers_my_page_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  
+  protected
+  
+  def configure_permitted_parameters
+    added_attrs = [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :encrypted_password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
