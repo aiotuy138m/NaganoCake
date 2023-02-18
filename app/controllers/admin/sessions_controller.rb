9991706238192bfbd4d38d595943b9ce8a8568e4
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-02-18 12:37:58 +0000

    FIX header's admin link_to path

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -4,19 +4,23 @@ class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+   def new
+     super
+   end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   def create
+     super
+   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+   def destroy
+     super
+   end
+
+   def after_sign_out_path_for(resource)
+      about_path
+   end
 
   # protected
 
