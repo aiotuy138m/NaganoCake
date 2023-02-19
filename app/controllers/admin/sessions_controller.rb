5c2e1059065c0ebf95492fbd973ea17b9c49501b
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-02-19 12:28:53 +0000

    UPDATE admin's top view

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -19,7 +19,7 @@ class Admin::SessionsController < Devise::SessionsController
    end
 
    def after_sign_out_path_for(resource)
-      about_path
+      new_admin_session_path
    end
 
   # protected
