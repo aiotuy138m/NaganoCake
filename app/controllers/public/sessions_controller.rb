9a163ce2423c7897cc3dd787f2a9843b91848e75
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-02-19 15:26:19 +0000

    [WIP] UPDATE customer's session

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  # before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
    def new
@@ -21,7 +22,7 @@ class Public::SessionsController < Devise::SessionsController
    def after_sign_in_path_for(resource)
      customers_my_page_path
    end
-   
+
    def after_sign_out_path_for(resource)
      about_path
    end
@@ -32,4 +33,15 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  protected
+  # 退会しているかを判断するメソッド
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+       redirect_to new_customer_session_path
+    if @customer.valid_password?(params[:customer][:password])
+      
+    end
+  end
+
 end
