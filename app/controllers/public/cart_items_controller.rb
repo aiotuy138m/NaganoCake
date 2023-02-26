0654d208c031f0de8c47933e027bc37edccfb726
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-02-26 07:29:40 +0000

    [UPDATE] cartitem add

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,16 +9,16 @@ class Public::CartItemsController < ApplicationController
     @cart_item.customer_id = current_customer.id
     @cart_items = current_customer.cart_items
     @cart_item.item_id = @item.id
-   #  binding.pry
-   # @cart_items.each do |cart_item|
-    #  if cart_item.item_id == @cart_item.item_id
-     #   new_amount = cart_item.amount + @cart_item.amount
-      #  cart_item.update_attribute(:amount, new_amount)
-       # @cart_item.delete
-    #  end
-  #  end
-      @cart_item.save
-      redirect_to cart_items_path
+    @cart_items.each do |cart_item|
+      if cart_item.item_id == @cart_item.item_id
+        new_amount = (cart_item.amount).to_i + @cart_item.amount
+        cart_item.update_attribute(:amount, new_amount)
+        @cart_item.delete
+      else
+        @cart_item.save
+      end
+    end
+    redirect_to cart_items_path
   end
 
 
@@ -30,7 +30,7 @@ class Public::CartItemsController < ApplicationController
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_items = current_customer.cart_items
-    if @cart_item.update(params[:id].to_i)
+    if @cart_item.update(params[:id])
       redirect_to cart_items_path
     else
       render :index
@@ -56,7 +56,7 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
 
 end
