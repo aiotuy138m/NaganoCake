b3d6d40922b97419f21cea59e0687de8c49bfa3c
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-03-01 08:10:20 +0000

    [WIP] order view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,19 +1,37 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @customer = current_customer
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to orders_confirm_path
+    else
+      render new_order_path
+    end
+  end
+
+  def index
+    @orders = current_customer.orders
+  end
+
+  def show
   end
 
   def confirm
+    @orders = current_customer.orders
   end
 
   def complete
   end
 
-  def create
+
+  private
+
+  def order_params
+    params.require(:order).permit(:post_code, :address, :name, :payment_way)
   end
 
-  def index
-  end
-
-  def show
-  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,9 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :addresses, dependent: :destroy
+  
+  def customer_address_display
+    '〒' + postal_code + ' ' + address + ' ' + last_name + first_name
+  end
+  
 end
