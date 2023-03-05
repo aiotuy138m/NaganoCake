f6931ef94ac2872a69fda9bf0b76134ba40b4028
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-03-05 14:11:42 +0000

    [UPDATE] admin order show view

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,16 +2,13 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @customer = Customer.find(@order.customer_id)
+    @postage = 800
   end
 
   def update
-    if @order = Order.find(params[:id])
-      @order.update(order_params)
-      return
-    elsif @order_detail = OrderDetail.find(params[:id])
-      @order_detail.update(order_detail_params)
+    @order = Order.find(params[:id])
+    @order.update(order_params)
     redirect_to admin_order_path
-    end
   end
   
   private
@@ -20,7 +17,4 @@ class Admin::OrdersController < ApplicationController
     params.require(:order).permit(:status)
   end
   
-  def order_detail_params
-    params.require(:order_detail).permit(:making_status)
-  end
 end
