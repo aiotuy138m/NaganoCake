cc7115f4e9c09f421a49b937b2ae9c96d62324a1
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-03-05 10:01:12 +0000

    [WIP] admin order show view

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,26 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @customer = Customer.find(@order.customer_id)
   end
 
   def update
+    if @order = Order.find(params[:id])
+      @order.update(order_params)
+      return
+    elsif @order_detail = OrderDetail.find(params[:id])
+      @order_detail.update(order_detail_params)
+    redirect_to admin_order_path
+    end
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
+  end
+  
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -6,4 +6,6 @@ class OrderDetail < ApplicationRecord
     item.with_tax_price * amount
   end
   
+  enum making_status: { unproduced: 0, waiting_for_production: 1, production: 2, production_complete: 3 }
+
 end
